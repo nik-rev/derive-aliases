06015b41cd4e539304ab8d35492f6a01c3a22546
Nik Revenco <pm@nikrev.com>
2025-09-26 15:06:03 +0100

    docs: add a lengthy explanation of the `new_alias!` macro

--- src/lib.rs
+++ src/lib.rs
@@ -4,73 +4,290 @@
 #[doc(inline)]
 pub use derive_aliases_proc_macro::{define, derive};
 
-/// # Glyphs
+/// This is the main macro that makes `derive_aliases` possible
 ///
-/// - `%`: Inject this alias's expansion into another `__internal_new_alias` call
+/// At a high level:
 ///
-/// With these aliases:
+/// - Invocation of `new_alias!` creates a new alias, which is a `macro_rules!`.
+///   So an invocation of `derive_aliases::define!` expands to a bunch of invocations of `new_alias!` each of
+///   which expand to `macro_rules!` alias
 ///
-/// The following aliases:
+/// - The `#[derive_aliases::derive]` macro takes the item it is applied to `:item` and nests it inside of a
+///   bunch of `macro_rules!` aliases located at `crate::derive_alias`
+///
+/// All of this must happen while keeping compile-times **extremely fast**.
+///
+/// # Example
+///
+/// Here's the simplest possible example. It defines an alias `..Eq` which expands to `Eq, PartialEq`. This macro will be invoked from `crate::derive_alias` module.
+///
+/// ```ignore
+///  derive_aliases::define! {
+///      Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
+///  }
+/// ```
+///
+/// The first expansion is this:
+///
+/// ```ignore
+/// ::derive_aliases::__internal_new_alias! {
+///     "..." __derive_alias_Eq $ Eq! [::core::cmp::PartialEq], [::core::cmp::Eq],
+/// }
+/// pub use __derive_alias_Eq as Eq;
+/// ```
+///
+/// - `"..."` contains the documentation, stuff passed to `#[doc = "..."]` so user gets a nice overview of what happens on hover,
+///   but it is pretty long and so we omit it
+///
+/// - `__derive_alias_Eq` is the real name of the generated `macro_rules!` alias. Because we must `#[macro_export]` it so it will
+///   be at the crate root (but we want to pretend it isn't), we have to do this. The `pub use __derive_alias_Eq as Eq` makes it available
+///   as `crate::derive_alias::Eq`, which is the actual path we reference when we later expand an alias like `#[derive_aliases::derive(..Eq)]`
+///
+/// - Because `new_alias!` itself creates a `macro_rules!` we need the `$` token inside of the `macro_rules!` definition. But we can't use `$`
+///   since that refers to the meta-variables captured by `new_alias!` and now the macro created *inside* `new_alias!`. So we do the 2nd
+///   best thing and have `$_:tt` which is always `$`. This allows us to use `$_` instead of `$` inside of the macro definition
+///
+/// - Next is `Eq!`, which is the real name of the macro that we'll `pub use ... as Eq`. The `macro_rules!` alias defined by `new_alias!` will
+///   refer to *itself* at exactly that position, as it will do some recursive TT munching by calling itself
+///
+/// - Next we have a bunch of paths inside of `[]`: `[$($path:tt)*]`. These paths are the *fully resolved* paths that the alias itself expands to.
+///   They do *not* reference another alias. So given this:
+///
+///   ```ignore
+///    derive_aliases::define! {
+///        Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
+///        Ord = ::core::cmp::Ord, ::core::cmp::PartialOrd, ..Eq;
+///    }
+///   ```
+///
+///   The `Ord` alias will expand to an invocation of `new_alias!` like this:
+///
+///   ```ignore
+///   ::derive_aliases::__internal_new_alias! {
+///       "..." __derive_alias_Ord $ Ord! [::core::cmp::PartialEq], [::core::cmp::Eq], [::core::cmp::Ord], [::core::cmp::PartialOrd],
+///   }
+///   pub use __derive_alias_Ord as Ord;
+///   ```
+///
+///   Because the `define!` macro itself referenes alias `..Eq` which it itself defines, we are able to "inline" all the aliases
+///   to make it more performant. However, we also allow to refer to aliases that were **not** defined inside of the same `define!` call.
+///   I'll explain how this is possible later.
+///
+///   **Back to `[$($path:tt)*]`**. We essentally use that instead of `$path:path` because you cannot compare 2 `:path` meta-variables. it's
+///   as simple as that. We **must** compare them, but we can't. But we CAN compare 1 `[$($path:tt)*]` with another `[$($path:tt)*]`
+///
+///   **Why we need to compare**: Our aliases have the important property that they act as *sets*. 2 aliases that expand to a bunch of derives,
+///   both expansions contain e.g. `::core::clone::Clone` derive. **But they can be used together**. They are merged like `HashSet`s.
+///
+///   **How the merger happens**: It's a recursion. Essentially, if our CURRENT alias adds `A` and `B` but we have `A, B, C, D`. We recurse to remove
+///   `A` and `B` from the set to get `C, D`. Then we add `A, B` again to get `A, B, C, D`. If any of `A` or `B` already existed, they won't be added
+///   again. If they didn't, they will be added now
+///
+/// Let's look at what `new_alias!` expands to now:
+///
+/// ```ignore
+/// ::derive_aliases::__internal_new_alias! {
+///     "..." __derive_alias_Eq $ Eq! [::core::cmp::PartialEq], [::core::cmp::Eq],
+/// }
+/// pub use __derive_alias_Eq as Eq;
+/// ```
+///
+/// The above expands into this:
+///
+/// ```ignore
+/// macro_rules! __derive_alias_Eq { /* ... */ }
+/// pub use __derive_alias_Eq as Eq;
+/// ```
+///
+/// The insides of this `macro_rules!` alias are not shown, as it's pretty big. And it won't make much sense to try and
+/// comprehend the generated `macro_rules!` alias. So instead let's see what an invocation of `#[derive]` expands to:
+///
+/// ```ignore
+/// #[derive(Debug, ..Eq)]
+/// struct Example;
+/// ```
+///
+/// Expansion is this:
+///
+/// ```ignore
+/// crate::derive_alias::Eq! { @[Debug,] [struct Example;] [] }
+/// ```
+///
+/// 1. We collect all normal non-alias derives inside of `[...]`, so `Debug, Clone` will be collected as `Debug, Clone,`
+/// 1. The actual `:item` that we apply this to is wrapped inside of `[]` entirely. This is incredibly efficient - we do **not**
+///    parse the `:item` itself, so it is just `:tt`. We pass this `:tt` everywhere. It is a SINGLE `TokenTree`, so it is very, very
+///    efficient to just pass it around. We only match on it as `[$($tt:tt)*]` to actually apply the alias at the END.
+///
+///    This is one of the KEY things that keeps compile-time for this macro extremely fast. We **do not** want to add any overhead **at all**.
+/// 1. Finally we have the empty `[]`. this is the **accumulator**. As we resolve more and more derives, we'll add all of them to this `[]`
+///    After we've resolved all the derives, we take all the things inside this `[]` and pass it to `#[std::deive]`.
+/// 1. `crate::derive_alias::Eq!` is referencing the actual `macro_rules!` alias that we explained earlier. This macro knows that
+///    it expands to derives `::core::cmp::PartialEq` and `::core::cmp::Eq` so it will add them to the `[]` accumulator mentioned previously
+/// 1. The `@` is just a glyph to keep compile times very fast. The insides of `macro_rules!` alias `Eq` contain a lot of rules, but
+///    since we start with `@` Rust's algorithm can discard all of the rules that do not match the first token, making compile-times very fast
+///
+/// Let's examine every stage of the expansion of the above:
+///
+/// ```ignore
+/// crate::derive_alias::Eq! { @[Debug,] [struct Example;] [] }
+///
+/// crate::derive_alias::Eq! { ?[Debug,][struct Example;][][] }
+///
+/// #[::core::prelude::v1::derive(Debug, ::core::cmp::PartialEq, ::core::cmp::Eq)]
+/// struct Example;
+/// ```
+///
+/// # Example with derives that conflict
+///
+/// As you can see this expanion is **very** simple because it just directly expands to the `std::derive`.
+/// However, it gets a lot more interesting when there are multiple aliases involved, and these aliases may contain
+/// derives that are overlapping and should be merged.
+///
+/// Given these aliases:
 ///
 /// ```ignore
 /// derive_aliases::define! {
-///     Copy = Copy, Clone, Eq;
-///     Eq = Eq, PartialEq, std::hash::Hash;
-///     Ord = Ord, PartialOrd, ..Eq, std::hash::Hash;
+///     Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
+///     Ord = ::core::cmp::Ord, ::core::cmp::PartialOrd, ..Eq;
 /// }
 /// ```
 ///
-/// Expand to the following invocations of `__internal_new_alias!`:
+/// They'll expand to this:
 ///
 /// ```ignore
-///  __internal_new_alias!(
-///      "a string which documents alias `Copy`" __derive_alias_Copy $ Copy! [Copy], [Clone], [Eq],
-///  );
-///  __internal_new_alias!(
-///      "a string which documents alias `Eq`" __derive_alias_Eq $ Eq! [Eq], [PartialEq], [std::hash::Hash],
-///  );
-///  __internal_new_alias!(
-///      "a string which documents alias `Ord`" __derive_alias_Ord $ Ord! [Ord], [PartialOrd], [Eq], [PartialEq], [std::hash::Hash],
-///  );
+/// ::derive_aliases::__internal_new_alias! {
+///     "..." __derive_alias_Ord $ Ord! [::core::cmp::PartialEq], [::core::cmp::Eq], [::core::cmp::Ord], [::core::cmp::PartialOrd],
+/// }
+/// pub use __derive_alias_Ord as Ord;
+///
+/// ::derive_aliases::__internal_new_alias! {
+///     "..." __derive_alias_Eq $ Eq! [::core::cmp::PartialEq], [::core::cmp::Eq],
+/// }
+/// pub use __derive_alias_Eq as Eq;
 /// ```
 ///
-/// Each invocation expands to a macro after the `$`. This macro **is the real derive alias**.
+/// Then expanding these aliases:
 ///
-/// Derive alias `macro_rules!` has the following properties:
+/// ```
+/// #[derive(Debug, ..Eq, ..Ord)]
+/// struct Example;
+/// ```
 ///
-/// - They can be composed with another derive alias
-/// - Composing them means merging the **sets** of derives that they expand to.
-///   What this means is 2 aliases that share derives they expand to will **merge** together.
+/// Will expand to this:
+///
+/// ```
+/// crate::derive_alias::Ord! { crate::derive_alias::Eq,(@[Debug,] [struct Example;]) [] }
+/// ```
+///
+/// Now it's a little more interesting. We are nesting the aliases 1 inside the other. Let's examine
+/// every state of their expansions (with `trace_macros!(true)`):
 ///
 /// ```ignore
-/// #[derive_aliases::derive(..Ord, ..Eq, ..Copy, Debug)]
-/// struct Foo;
+/// crate::derive_alias::Ord! { crate::derive_alias::Eq,(@[Debug,] [struct Example;]) [] }
+/// crate::derive_alias::Ord! { #crate::derive_alias::Eq,(@[Debug,] [struct Example;]) [] [] }
+///
+/// // first, the Ord! simply inserts all of its expansions into our stack
+/// crate::derive_alias::Eq! { @[Debug,] [struct Example;] [[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::Ord],[::core::cmp::PartialOrd],] }
+///
+/// // alias `Eq` expands to `::core::cmp::Eq` and `::core::cmp::PartialEq`. Because these can't just be added (they conflict with each-other)
+/// // we go through all existing derives in our stack and remove all of those that are exactly `::core::cmp::PartialEq` and `::core::cmp::Eq`
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::Ord],[::core::cmp::PartialOrd],] [] }
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::Ord],[::core::cmp::PartialOrd],] [] }
+///
+/// // Removed `PartialEq`
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [[::core::cmp::Eq],[::core::cmp::Ord],[::core::cmp::PartialOrd],] [] }
+///
+/// // Removed `Eq`
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [[::core::cmp::Ord],[::core::cmp::PartialOrd],] [] }
+///
+/// // Did NOT remove `Ord`, added it to the "good" pile (no conflicts)
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [[::core::cmp::PartialOrd],] [[::core::cmp::Ord],] }
+///
+/// // Did NOT remove `PartialOrd`, added it to the "good" pile (no conflicts)
+/// crate::derive_alias::Eq! { ?[Debug,] [struct Example;] [] [[::core::cmp::Ord],[::core::cmp::PartialOrd],] }
+///
+/// // Now that our pile is totally empty, and we reached end of the expansion (signified by the `?` glyph)
+/// // let's generate the real `#[std::derive]`
+/// #[::core::prelude::v1::derive(Debug,::core::cmp::Ord,::core::cmp::PartialOrd,::core::cmp::PartialEq,::core::cmp::Eq,)] struct Example;
 /// ```
 ///
-/// Expands to this:
+/// What we learned:
+///
+/// 1. Each additional alias adds an extra level of nesting
+/// 1. We resolve aliases 1-at-a-time. Each alias knows what derives it expands to.
+/// 1. Each alias REMOVES all the same aliases that exist from the list of aliases to expand to.
+/// 1. Once done, it adds it aliases to the end of the list
+/// 1. The process is repeated for each alias
+///
+/// # External aliases
+///
+/// We can do the "inlining" of aliases pretty easily when they are defined in the same `derive_aliases::define!` macro.
+/// However, when the aliases **are not** defined like this, (they might come from an external crate or module) - we must reference
+/// them by their path, e.g. `Ord`  would  be referenced as `crate::derive_alias::Eq`.
 ///
 /// ```ignore
-/// crate::derive_alias::Ord! ( crate::derive_alias::Eq,(crate::derive_alias::Copy,(@ [Debug,] [struct Foo;])) [] );
+/// derive_aliases::define! {
+///     Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
+/// }
+/// derive_aliases::define! {
+///     Ord = ::core::cmp::Ord, ::core::cmp::PartialOrd, ..Eq;
+///     Ord2 = ::core::cmp::Ord, ::core::cmp::PartialOrd, ..Eq, ..Eq;
+///     Ord3 = ::core::cmp::Ord, ::core::cmp::PartialOrd, ..Eq, ..Eq, ..Eq;
+/// }
 /// ```
 ///
-/// # Notes
-///
-/// - In order to **merge** sets of derives, we don't use the `:path` specifier since it cannot be compared to another `:path`.
-///   Instead, we store paths to derives as `[$(:tt)*]` which we CAN compare with one another
-///
-/// Creating stacked aliases looks like this:
+/// The 1nd `Ord` expands like this:
 ///
 /// ```ignore
-/// Ord! (%
-///     [Ord,
-///         [Ord,
-///             [
-///                 "Derive alias `..AndMore` can be used like this:\n\n```ignore\n#[derive(..AndMore)]\nstruct Example;\n```\n\nWhich expands to the following:\n\n```ignore\n#[derive(Clone, Copy, Default, Hash)]\nstruct Example;\n\nuse core::hash::Hash\n```" __derive_alias_AndMore$AndMore![::core::default::Default],[::core::marker::Copy],[::core::clone::Clone],[::std::hash::Hash],
-///             ]
-///         ]
-///     ]
-/// );
+/// // 1 external alias
+/// crate::derive_alias::Eq! {%
+///     ["..." __derive_alias_Ord$Ord![::core::cmp::Ord],[::core::cmp::PartialOrd],]
+/// }
+/// // expansion:
+/// __internal_new_alias! {%
+///     "..." __derive_alias_Ord$Ord![::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
 /// ```
+///
+/// The 2nd `Ord2` expands like this:
+///
+/// ```ignore
+/// // 2 external aliases
+/// crate::derive_alias::Eq! {%
+///     [crate::derive_alias::Eq,["..." __derive_alias_Ord2 $ Ord2![::core::cmp::Ord],[::core::cmp::PartialOrd],]]
+/// }
+/// // expansion:
+/// crate::derive_alias::Eq! {%
+///     ["..." __derive_alias_Ord2 $ Ord2! [::core::cmp::Ord],[::core::cmp::PartialOrd],][::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
+/// // expansion:
+/// crate::__internal_new_alias! {
+///     "..." __derive_alias_Ord2 $ Ord2! [::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
+/// ```
+///
+/// The 3rd `Ord3` expands like this:
+///
+/// ```ignore
+/// // 3 external aliases
+/// crate::derive_alias::Eq! {%
+///     [crate::derive_alias::Eq,[crate::derive_alias::Eq,["..." __derive_alias_Ord 3 $Ord3![::core::cmp::Ord],[::core::cmp::PartialOrd],]]]
+/// }
+/// // expansion:
+/// crate::derive_alias::Eq! {%
+///     [crate::derive_alias::Eq,["..." __derive_alias_Ord3 $ Ord3![::core::cmp::Ord],[::core::cmp::PartialOrd],]][::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
+/// // expansion:
+/// crate::derive_alias::Eq! {%
+///     ["..." __derive_alias_Ord3 $ Ord3![::core::cmp::Ord],[::core::cmp::PartialOrd],][::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
+/// // expansion:
+/// __internal_new_alias! {
+///     "..." __derive_alias_Ord3 $ Ord3![::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],
+/// }
+/// ```
+///
+/// As you can see, each nesting of `new_alias!` inside another alias just inserts additional `[$($path:tt)],` paths at the end. The `%` glyph tells the alias to do exactly this.
 #[doc(hidden)]
 #[macro_export]
 macro_rules! __internal_new_alias {
@@ -91,7 +308,7 @@ macro_rules! __internal_new_alias {
     ) => {
         #[doc = $docs]
         #[macro_export]
-        #[allow(crate_in_macro_def)]
+        #[allow(clippy::crate_in_macro_def)]
         #[doc(hidden)]
         macro_rules! $real_name {
             ///////////////////////////////////////////////////////////////
--- tests/test.rs
+++ tests/test.rs
@@ -1,55 +1,83 @@
-macro_rules! assert_impls {
-    ($name:ident [$($input:tt)*] => $($($segment:ident)::*),*) => {
-        mod $name {
-            #[derive_aliases::derive($($input)*)]
-            pub struct A(());
-        }
-
-        // HACK: since we can't use `+` as repetition separator
-        #[allow(warnings, reason = "only for type check")]
-        fn $name<A: $($($segment)::* +)* Sized>() {
-            $name::<$name::A>();
-        }
-    };
-}
-
 mod derive_alias {
-    mod foo {
-        derive_aliases::define! {
-            Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
-            AndMore = ::std::hash::Hash, ..Ord, ::core::clone::Clone, ::core::marker::Copy, ::core::default::Default;
-        }
+    derive_aliases::define! {
+        Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
     }
-    mod bar {
-        derive_aliases::define! {
-            Ord = ::core::cmp::PartialOrd, ::core::cmp::Ord, ..Eq;
-
-            Everything = ::std::hash::Hash, ..Ord, ::core::marker::Copy, ::core::clone::Clone, ::core::default::Default;
-        }
+    crate::__internal_new_alias! {
+        "..." __derive_alias_Ord$Ord![::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],
     }
-
-    pub use bar::{Everything, Ord};
-    pub use foo::*;
+    pub use __derive_alias_Ord as Ord;
+    crate::__internal_new_alias! {
+        "..." __derive_alias_Ord2$Ord2![::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],
+    }
+    pub use __derive_alias_Ord2 as Ord2;
+    crate::__internal_new_alias! {
+        "..." __derive_alias_Ord3$Ord3![::core::cmp::Ord],[::core::cmp::PartialOrd],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],[::core::cmp::PartialEq],[::core::cmp::Eq],
+    }
+    pub use __derive_alias_Ord3 as Ord3;
 }
 
-assert_impls!(a [Clone] => Clone);
-assert_impls!(b [..Eq] => Eq, PartialEq);
-assert_impls!(c [..AndMore] => PartialOrd, Ord, PartialEq, Eq, Copy, Clone, Default, std::hash::Hash);
-assert_impls!(d [..Ord] => PartialOrd, Ord, PartialEq, Eq);
-assert_impls!(e [..Everything] => std::hash::Hash, PartialOrd, Ord, Eq, PartialEq, Copy, Clone, Default);
+// trace_macros!(true);
 
-assert_impls!(f [..Eq, Clone] => Eq, PartialEq, Clone);
-assert_impls!(g [Clone, ..Eq] => Eq, PartialEq, Clone);
+// #[derive_aliases::derive(Debug, ..Eq, ..Ord)]
+// struct Example;
 
-assert_impls!(h [Clone, ..Eq, Copy] => Eq, PartialEq, Clone, Copy);
-assert_impls!(i [..Eq, Clone, Copy] => Eq, PartialEq, Clone, Copy);
-assert_impls!(j [Clone, Copy, ..Eq] => Eq, PartialEq, Clone, Copy);
+// #[derive_aliases::derive(Debug, ..Eq)]
+// struct Example;
 
-assert_impls!(m [..Eq, ..Eq] => Eq, PartialEq);
+// compile_error!("lol");
 
-#[allow(unused_imports)]
-use derive_alias::Everything;
+// macro_rules! assert_impls {
+//     ($name:ident [$($input:tt)*] => $($($segment:ident)::*),*) => {
+//         mod $name {
+//             #[derive_aliases::derive($($input)*)]
+//             pub struct A(());
+//         }
 
-#[cfg(test)]
-#[test]
-fn it_compiles() {}
+//         // HACK: since we can't use `+` as repetition separator
+//         #[allow(warnings, reason = "only for type check")]
+//         fn $name<A: $($($segment)::* +)* Sized>() {
+//             $name::<$name::A>();
+//         }
+//     };
+// }
+
+// mod derive_alias {
+//     mod foo {
+//         derive_aliases::define! {
+//             Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
+//             AndMore = ::std::hash::Hash, ..Ord, ::core::clone::Clone, ::core::marker::Copy, ::core::default::Default;
+//         }
+//     }
+//     mod bar {
+//         derive_aliases::define! {
+//             Ord = ::core::cmp::PartialOrd, ::core::cmp::Ord, ..Eq;
+
+//             Everything = ::std::hash::Hash, ..Ord, ::core::marker::Copy, ::core::clone::Clone, ::core::default::Default;
+//         }
+//     }
+
+//     pub use bar::{Everything, Ord};
+//     pub use foo::*;
+// }
+
+// assert_impls!(a [Clone] => Clone);
+// assert_impls!(b [..Eq] => Eq, PartialEq);
+// assert_impls!(c [..AndMore] => PartialOrd, Ord, PartialEq, Eq, Copy, Clone, Default, std::hash::Hash);
+// assert_impls!(d [..Ord] => PartialOrd, Ord, PartialEq, Eq);
+// assert_impls!(e [..Everything] => std::hash::Hash, PartialOrd, Ord, Eq, PartialEq, Copy, Clone, Default);
+
+// assert_impls!(f [..Eq, Clone] => Eq, PartialEq, Clone);
+// assert_impls!(g [Clone, ..Eq] => Eq, PartialEq, Clone);
+
+// assert_impls!(h [Clone, ..Eq, Copy] => Eq, PartialEq, Clone, Copy);
+// assert_impls!(i [..Eq, Clone, Copy] => Eq, PartialEq, Clone, Copy);
+// assert_impls!(j [Clone, Copy, ..Eq] => Eq, PartialEq, Clone, Copy);
+
+// assert_impls!(m [..Eq, ..Eq] => Eq, PartialEq);
+
+// #[allow(unused_imports)]
+// use derive_alias::Everything;
+
+// #[cfg(test)]
+// #[test]
+// fn it_compiles() {}
