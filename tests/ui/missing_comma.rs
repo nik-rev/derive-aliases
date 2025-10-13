992a0a44de2663e3bce74a583eee60443701a9cb
Nik Revenco <pm@nikrev.com>
2025-10-13 19:15:15 +0100

    test: Remove flakey tests

--- tests/ui/missing_comma.rs
+++
@@ -1,9 +0,0 @@
-pub mod derive_alias {
-    derive_aliases::define! {
-        Eq = ::core::cmp::Eq, ::core::cmp::PartialEq;
-        AndMore = ::std::hash::Hash  ..Ord, ::core::clone::Clone, ::core::marker::Copy, ::core::default::Default;
-        //                         ^ missing `,`
-    }
-}
-
-fn main() {}
