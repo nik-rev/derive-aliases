bce7c0fc85b9fb14eb766a8b1d8c233e57f46ecf
Nik Revenco <pm@nikrev.com>
2026-01-18 14:25:16 +0000

    Add more tests for helper attribute name resolution

---
+++ tests/helper_container.rs
@@ -0,0 +1,47 @@
+//! Test that multiple `derive_aliases::derive` attributes
+//! in a row won't mess up helper attribute name resolution
+//! on the container
+#![allow(unused)]
+
+mod derive_alias {
+    derive_aliases::define! {
+        Serialize = ::serde::Serialize;
+        Clone = ::core::clone::Clone;
+    }
+}
+
+#[derive_aliases::derive(..Serialize)]
+#[derive_aliases::derive(..Clone)]
+#[serde(deny_unknown_fields)]
+struct A {}
+
+#[derive_aliases::derive(..Clone)]
+#[derive_aliases::derive(..Serialize)]
+#[serde(deny_unknown_fields)]
+struct B {}
+
+// Clone can come afterwards because
+#[derive_aliases::derive(..Serialize)]
+#[serde(deny_unknown_fields)]
+#[derive_aliases::derive(..Clone)]
+struct C {}
+
+#[derive_aliases::derive(::serde::Serialize)]
+#[serde(deny_unknown_fields)]
+#[derive_aliases::derive(..Clone)]
+struct D {}
+
+#[derive(::serde::Serialize)]
+#[serde(deny_unknown_fields)]
+#[derive_aliases::derive(..Clone)]
+struct E {}
+
+#[derive_aliases::derive(..Serialize)]
+#[serde(deny_unknown_fields)]
+#[derive_aliases::derive(::core::clone::Clone)]
+struct F {}
+
+#[derive_aliases::derive(..Serialize)]
+#[serde(deny_unknown_fields)]
+#[derive(::core::clone::Clone)]
+struct G {}
